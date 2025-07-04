e0a09b9940d1a447ed5dfa771631b5294dd22f1b
Omar-dev-xponent <omar12.xponent@gmail.com>
2025-07-05 00:09:20 +0600

    test: husky

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,8 @@ process.on('uncaughtException', (err) => {
   logger.error(`Shutting down the server due to uncaught exception!`);
   process.exit(1);
 });
-x;
+const foo = 1;
+foo = 'foo';
 // server--
 const server = app.listen(process.env.PORT, () => {
   logger.info(`Server listening on port ${process.env.PORT}`);
